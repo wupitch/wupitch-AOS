25e1384f12c1fc485a167b17eaa386cb3ac949b8
EunjeongChoi414 <giving0414@naver.com>
2021-10-21 13:33:55 +0900

    약관동의 로직 구현

--- app/src/main/java/wupitch/android/presentation/signup/ServiceAgreementFragment.kt
+++ app/src/main/java/wupitch/android/presentation/signup/ServiceAgreementFragment.kt
@@ -13,62 +13,62 @@ import wupitch.android.common.BaseFragment
 import wupitch.android.databinding.FragmentServiceAgreementBinding
 
 class ServiceAgreementFragment
-    : BaseFragment<FragmentServiceAgreementBinding>(FragmentServiceAgreementBinding::bind, R.layout.fragment_service_agreement) {
+    : BaseFragment<FragmentServiceAgreementBinding>(
+    FragmentServiceAgreementBinding::bind,
+    R.layout.fragment_service_agreement
+) {
 
     private var agreementSatisfiedNum = 0
+    private var nonAllButtonClicked = true
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.fragment = this
         setTitleText()
 
-        binding.btnNext.setOnClickListener {
-            if(it.isActivated) Navigation.findNavController(it).navigate(R.id.action_serviceAgreementFragment_to_regionFragment)
+    }
+
+    val checkedChangeListener =
+        CompoundButton.OnCheckedChangeListener { _, isChecked ->
+            if (isChecked) ++agreementSatisfiedNum
+            else --agreementSatisfiedNum
         }
 
-        binding.toggleAgreeAll.setOnCheckedChangeListener { _, isChecked ->
-               if(isChecked) {
-                   agreementSatisfiedNum += 3
-                   binding.btnNext.isActivated = true
-
-                   binding.toggleServiceAgreement.isChecked = true
-                   binding.togglePrivacy.isChecked = true
-                   binding.togglePushNotification.isChecked = true
-               } else {
-                   agreementSatisfiedNum -= 3
-                   binding.btnNext.isActivated = false
-
-                   binding.toggleServiceAgreement.isChecked = false
-                   binding.togglePrivacy.isChecked = false
-                   binding.togglePushNotification.isChecked = false
-               }
+    fun setAllButtonClickListener () {
+        nonAllButtonClicked = false
+        if (!binding.toggleAgreeAll.isChecked && !nonAllButtonClicked) {
+            binding.btnNext.isActivated = false
+            binding.toggleServiceAgreement.isChecked = false
+            binding.togglePrivacy.isChecked = false
+            binding.togglePushNotification.isChecked = false
+        }else {
+            binding.btnNext.isActivated = true
+            binding.toggleServiceAgreement.isChecked = true
+            binding.togglePrivacy.isChecked = true
+            binding.togglePushNotification.isChecked = true
         }
+        checkIfAgreementSatisfied()
+    }
 
-        val checkedChangeListener =
-            CompoundButton.OnCheckedChangeListener { _, isChecked ->
-                if(isChecked) {
-                    ++agreementSatisfiedNum
-                }else {
-                    --agreementSatisfiedNum
-                }
-                checkIfAgreementSatisfied()
-            }
-
-        binding.toggleServiceAgreement.setOnCheckedChangeListener(checkedChangeListener)
-        binding.togglePrivacy.setOnCheckedChangeListener(checkedChangeListener)
-        binding.togglePushNotification.setOnCheckedChangeListener(checkedChangeListener)
-
-
-
+    fun setNonAllButtonClickListener () {
+        nonAllButtonClicked = true
+        checkIfAgreementSatisfied()
     }
 
     private fun checkIfAgreementSatisfied() {
-         binding.btnNext.isActivated = agreementSatisfiedNum>=2
-        binding.toggleAgreeAll.isChecked = agreementSatisfiedNum >=3
-        Log.d("{ServiceAgreementFragment.checkIfAgreementSatisfied}", agreementSatisfiedNum.toString())
+        binding.btnNext.isActivated =
+            (binding.toggleServiceAgreement.isChecked && binding.togglePrivacy.isChecked) || binding.toggleAgreeAll.isChecked
+        binding.toggleAgreeAll.isChecked = agreementSatisfiedNum >= 3
     }
 
     private fun setTitleText() {
-        binding.tvServiceAgreementTitle.text = Html.fromHtml(getString(R.string.service_agreement_title))
+        binding.tvServiceAgreementTitle.text =
+            Html.fromHtml(getString(R.string.service_agreement_title))
+    }
+
+    fun checkForNavigationToRegion(view: View) {
+        if (view.isActivated) Navigation.findNavController(view)
+            .navigate(R.id.action_serviceAgreementFragment_to_regionFragment)
     }
 }
