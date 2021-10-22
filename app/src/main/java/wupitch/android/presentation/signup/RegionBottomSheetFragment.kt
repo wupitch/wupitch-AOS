3913ea562cf05d410e0afb89dccbd28423a7b3b5
EunjeongChoi414 <giving0414@naver.com>
2021-10-23 01:41:11 +0900

    지역 fragment && 바텀시트 화면 구현

---
+++ app/src/main/java/wupitch/android/presentation/signup/RegionBottomSheetFragment.kt
@@ -0,0 +1,58 @@
+package wupitch.android.presentation.signup
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import wupitch.android.R
+import wupitch.android.common.BaseFragment
+import wupitch.android.databinding.FragmentRegionBottomSheetBinding
+
+class RegionBottomSheetFragment
+    : BottomSheetDialogFragment()
+{
+
+    private var _binding: FragmentRegionBottomSheetBinding? = null
+    private val binding get() = _binding!!
+    private val regionList = arrayOf<String>(
+        "서울시", "도봉구", "노원구", "강북구", "성북구", "은평구", "종로구", "동대문구",
+        "중랑구", "서대문구", "중구", "성동구", "광진구", "마포구", "용산구", "강서구",
+        "양천구", "구로구", "영등포구", "동작구", "관악구", "금천구", "서초구", "강남구",
+        "송파구", "강동구"
+    )
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme);
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRegionBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.pickerRegion.apply {
+            minValue = 0
+            maxValue = regionList.size -1
+            binding.pickerRegion.displayedValues = regionList
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/wupitch/android/presentation/signup/RegionFragment.kt
+++ app/src/main/java/wupitch/android/presentation/signup/RegionFragment.kt
@@ -2,6 +2,7 @@ package wupitch.android.presentation.signup
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.Navigation
 import wupitch.android.R
 import wupitch.android.common.BaseFragment
 import wupitch.android.databinding.FragmentRegionBinding
@@ -10,8 +11,21 @@ import wupitch.android.databinding.FragmentServiceAgreementBinding
 class RegionFragment
     : BaseFragment<FragmentRegionBinding>(FragmentRegionBinding::bind, R.layout.fragment_region) {
 
+    private lateinit var regionBottomSheetFragment: RegionBottomSheetFragment
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.fragment = this
+    }
+
+    fun showRegionBottomSheet() {
+        regionBottomSheetFragment = RegionBottomSheetFragment()
+        regionBottomSheetFragment.show(childFragmentManager, "region_bottom_sheet")
+    }
+
+    fun checkForNavigationToSport(view: View) {
+        if (view.isActivated) Navigation.findNavController(view)
+            .navigate(R.id.action_regionFragment_to_sportFragment)
     }
 }
