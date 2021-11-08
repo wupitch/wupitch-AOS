4659970d7ef6a00da22ffa913bacb73d62045759
EunjeongChoi414 <giving0414@naver.com>
2021-11-08 16:49:56 +0900

    이용약관 상세 compose 변환

--- app/src/main/java/wupitch/android/presentation/ui/signup/ServiceAgreementDetailFragment.kt
+++ app/src/main/java/wupitch/android/presentation/ui/signup/ServiceAgreementDetailFragment.kt
@@ -1,17 +1,77 @@
 package wupitch.android.presentation.ui.signup
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Divider
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import wupitch.android.R
 import wupitch.android.common.BaseFragment
+import wupitch.android.presentation.theme.WupitchTheme
+import wupitch.android.presentation.ui.components.SetToolBar
 
-class ServiceAgreementDetailFragment
-    :Fragment() {
+class ServiceAgreementDetailFragment :Fragment() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                WupitchTheme {
+                    ConstraintLayout(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.White)
+                    ) {
+                        val (toolbar, divider) = createRefs()
 
+                        SetToolBar(modifier = Modifier.constrainAs(toolbar) {
+                            top.linkTo(parent.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        }, onLeftIconClick = {
+                            findNavController().navigateUp()
+                        }, textString =  R.string.terms_of_service_agreement,
+                            hasRightIcon = true,
+                            onRightIconClick = {
+                                findNavController().navigateUp()
+                            }
+                        )
+
+                        Divider(
+                            modifier = Modifier
+                                .constrainAs(divider){
+                                    top.linkTo(toolbar.bottom)
+                                }
+                                .fillMaxWidth()
+                                .height(1.dp)
+                                .background(
+                                    colorResource(
+                                        id = R.color.gray01
+                                    )
+                                )
+                        )
+                    }
+
+                }
+            }
+        }
     }
 }
--- app/src/main/java/wupitch/android/presentation/ui/signup/UseOfPersonalInfoDetailFragment.kt
+++ app/src/main/java/wupitch/android/presentation/ui/signup/UseOfPersonalInfoDetailFragment.kt
@@ -1,18 +1,74 @@
 package wupitch.android.presentation.ui.signup
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Divider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import wupitch.android.R
 import wupitch.android.common.BaseFragment
+import wupitch.android.presentation.theme.WupitchTheme
+import wupitch.android.presentation.ui.components.SetToolBar
 
-class UseOfPersonalInfoDetailFragment : Fragment()
-{
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+class UseOfPersonalInfoDetailFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                WupitchTheme {
+                    ConstraintLayout(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.White)
+                    ) {
+                        val (toolbar, divider) = createRefs()
 
+                        SetToolBar(modifier = Modifier.constrainAs(toolbar) {
+                            top.linkTo(parent.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        }, onLeftIconClick = {
+                            findNavController().navigateUp()
+                        }, textString = R.string.terms_of_privacy_policy,
+                            hasRightIcon = true,
+                            onRightIconClick = {
+                                findNavController().navigateUp()
+                            }
+                        )
 
+                        Divider(
+                            modifier = Modifier
+                                .constrainAs(divider) {
+                                    top.linkTo(toolbar.bottom)
+                                }
+                                .fillMaxWidth()
+                                .height(1.dp)
+                                .background(
+                                    colorResource(
+                                        id = R.color.gray01
+                                    )
+                                )
+                        )
+                    }
+
+                }
+
+            }
+        }
     }
-
 }
