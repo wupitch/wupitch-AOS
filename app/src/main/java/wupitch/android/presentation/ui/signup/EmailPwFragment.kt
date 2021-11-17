be7632420259d420737de2838a517a75e3d3d3ba
EunjeongChoi414 <giving0414@naver.com>
2021-11-17 23:32:31 +0900

    이메일비번 버그 수정

--- app/src/main/java/wupitch/android/presentation/ui/signup/EmailPwFragment.kt
+++ app/src/main/java/wupitch/android/presentation/ui/signup/EmailPwFragment.kt
@@ -1,9 +1,11 @@
 package wupitch.android.presentation.ui.signup
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -11,13 +13,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -30,6 +32,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -82,11 +85,16 @@ class EmailPwFragment : Fragment() {
                             textString = stringResource(id = R.string.warning_stop_signup)
                         )
                     }
-                    val emailTextState = remember { mutableStateOf("") }
-                    val pwTextState = remember { mutableStateOf("") }
+                    val emailTextState = remember { mutableStateOf(viewModel.userEmail.value?:"") }
+                    val pwTextState = remember { mutableStateOf(viewModel.userPw.value?:"") }
 
                     val isEmailValid = viewModel.isEmailValid.observeAsState().value
                     val isPwValid = viewModel.isPwValid.observeAsState().value
+
+                    val customTextSelectionColors = TextSelectionColors(
+                        handleColor = colorResource(id = R.color.gray03),
+                        backgroundColor = colorResource(id = R.color.gray03)
+                    )
 
 
                     ConstraintLayout(
@@ -137,6 +145,7 @@ class EmailPwFragment : Fragment() {
                                 }) {
                             EmailTextField(
                                 textState = emailTextState,
+                                customTextSelectionColors = customTextSelectionColors
                             )
                             Spacer(modifier = Modifier.height(6.dp))
 
@@ -161,7 +170,7 @@ class EmailPwFragment : Fragment() {
                             }
                             Spacer(modifier = Modifier.height(10.dp))
 
-                            PasswordTextField(pwTextState)
+                            PasswordTextField(pwTextState, customTextSelectionColors)
                             Spacer(modifier = Modifier.height(6.dp))
 
                             if (pwTextState.value.isNotEmpty() && isPwValid == false) {
@@ -207,87 +216,13 @@ class EmailPwFragment : Fragment() {
     @Composable
     fun EmailTextField(
         textState: MutableState<String>,
-    ) {
-        BasicTextField(
-            value = textState.value,
-            textStyle = TextStyle(
-                color = Color.Black,
-                fontSize = 16.sp,
-                fontFamily = Roboto,
-                fontWeight = FontWeight.Normal
-            ),
-            maxLines = 1,
-            onValueChange = { value ->
-                textState.value = value
-
-                emailJob?.cancel()
-                emailJob = lifecycleScope.launch {
-                    delay(1200L)
-                    viewModel.checkEmailValid(value)
-                }
-            },
-            cursorBrush = SolidColor(colorResource(id = R.color.gray03)),
-            decorationBox = { innerTextField ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(8.dp))
-                        .background(colorResource(id = R.color.gray04))
-                        .padding(horizontal = 16.dp)
-                        .padding(top = 11.dp, bottom = 9.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-
-                    ConstraintLayout(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(Color.Transparent)
-                    ) {
-                        val (hint) = createRefs()
-
-                        innerTextField()
-                        if (textState.value.isEmpty()) {
-                            Text(
-                                modifier = Modifier.constrainAs(hint) {
-                                    start.linkTo(parent.start)
-                                    top.linkTo(parent.top)
-                                    bottom.linkTo(parent.bottom)
-                                },
-                                text = stringResource(id = R.string.email),
-                                color = colorResource(id = R.color.gray03),
-                                fontSize = 16.sp,
-                                fontFamily = Roboto,
-                                fontWeight = FontWeight.Normal
-                            )
-                        }
-                    }
-                }
-            }
-        )
-    }
-
-    @ExperimentalPagerApi
-    @Composable
-    fun PasswordTextField(
-        textState: MutableState<String>
+        customTextSelectionColors: TextSelectionColors
     ) {
 
-        val pwViewToggleState = remember { mutableStateOf(false) }
+        CompositionLocalProvider(LocalTextSelectionColors provides customTextSelectionColors) {
 
-        ConstraintLayout(Modifier.fillMaxWidth()) {
-            val (textField, icon) = createRefs()
 
             BasicTextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .constrainAs(textField) {
-                        top.linkTo(parent.top)
-                        bottom.linkTo(parent.bottom)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    },
-                visualTransformation = if (pwViewToggleState.value) VisualTransformation.None else PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                 value = textState.value,
                 textStyle = TextStyle(
                     color = Color.Black,
@@ -299,13 +234,13 @@ class EmailPwFragment : Fragment() {
                 onValueChange = { value ->
                     textState.value = value
 
-                    pwJob?.cancel()
-                    pwJob = lifecycleScope.launch {
+                    emailJob?.cancel()
+                    emailJob = lifecycleScope.launch {
                         delay(1200L)
-                        viewModel.checkPwValid(value)
+                        viewModel.checkEmailValid(value)
                     }
-
                 },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
                 cursorBrush = SolidColor(colorResource(id = R.color.gray03)),
                 decorationBox = { innerTextField ->
                     Row(
@@ -313,12 +248,14 @@ class EmailPwFragment : Fragment() {
                             .fillMaxWidth()
                             .clip(RoundedCornerShape(8.dp))
                             .background(colorResource(id = R.color.gray04))
-                            .padding(start = 16.dp, top = 11.dp, bottom = 9.dp, end = 55.dp),
+                            .padding(horizontal = 16.dp)
+                            .padding(top = 11.dp, bottom = 9.dp),
                         verticalAlignment = Alignment.CenterVertically
                     ) {
 
                         ConstraintLayout(
                             modifier = Modifier
+                                .fillMaxWidth()
                                 .background(Color.Transparent)
                         ) {
                             val (hint) = createRefs()
@@ -331,10 +268,8 @@ class EmailPwFragment : Fragment() {
                                         top.linkTo(parent.top)
                                         bottom.linkTo(parent.bottom)
                                     },
-                                    text = stringResource(id = R.string.pw),
-                                    color = colorResource(
-                                        id = R.color.gray03
-                                    ),
+                                    text = stringResource(id = R.string.email),
+                                    color = colorResource(id = R.color.gray03),
                                     fontSize = 16.sp,
                                     fontFamily = Roboto,
                                     fontWeight = FontWeight.Normal
@@ -344,31 +279,123 @@ class EmailPwFragment : Fragment() {
                     }
                 }
             )
-
-            Image(
-                modifier = Modifier
-                    .constrainAs(icon) {
-                        top.linkTo(parent.top)
-                        bottom.linkTo(parent.bottom)
-                        end.linkTo(textField.end, margin = 20.dp)
-                        height = Dimension.fillToConstraints
-                    }
-                    .size(24.dp)
-                    .toggleable(
-                        value = pwViewToggleState.value,
-                        interactionSource = remember { MutableInteractionSource() },
-                        indication = null,
-                        enabled = true,
-                        role = Role.Checkbox,
-                        onValueChange = {
-                            pwViewToggleState.value = it
-                        }
-                    ),
-                painter = if (pwViewToggleState.value) painterResource(id = R.drawable.view)
-                else painterResource(id = R.drawable.view_hide),
-                contentDescription = "delete search text",
-            )
-
         }
+    }
+
+    @ExperimentalPagerApi
+    @Composable
+    fun PasswordTextField(
+        textState: MutableState<String>,
+        customTextSelectionColors: TextSelectionColors
+    ) {
+        val pwViewToggleState = remember { mutableStateOf(false) }
+
+        CompositionLocalProvider(LocalTextSelectionColors provides customTextSelectionColors) {
+
+            ConstraintLayout(Modifier.fillMaxWidth()) {
+                val (textField, icon) = createRefs()
+
+                BasicTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .constrainAs(textField) {
+                            top.linkTo(parent.top)
+                            bottom.linkTo(parent.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        },
+                    visualTransformation = if (pwViewToggleState.value) VisualTransformation.None else PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Done),
+                    keyboardActions = KeyboardActions (onDone = {
+                        setKeyboardDown()
+                    }),
+                    value = textState.value,
+                    textStyle = TextStyle(
+                        color = Color.Black,
+                        fontSize = 16.sp,
+                        fontFamily = Roboto,
+                        fontWeight = FontWeight.Normal
+                    ),
+                    maxLines = 1,
+                    onValueChange = { value ->
+                        textState.value = value
+
+                        pwJob?.cancel()
+                        pwJob = lifecycleScope.launch {
+                            delay(1200L)
+                            viewModel.checkPwValid(value)
+                        }
+
+                    },
+                    cursorBrush = SolidColor(colorResource(id = R.color.gray03)),
+                    decorationBox = { innerTextField ->
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(8.dp))
+                                .background(colorResource(id = R.color.gray04))
+                                .padding(start = 16.dp, top = 11.dp, bottom = 9.dp, end = 55.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+
+                            ConstraintLayout(
+                                modifier = Modifier
+                                    .background(Color.Transparent)
+                            ) {
+                                val (hint) = createRefs()
+
+                                innerTextField()
+                                if (textState.value.isEmpty()) {
+                                    Text(
+                                        modifier = Modifier.constrainAs(hint) {
+                                            start.linkTo(parent.start)
+                                            top.linkTo(parent.top)
+                                            bottom.linkTo(parent.bottom)
+                                        },
+                                        text = stringResource(id = R.string.pw),
+                                        color = colorResource(
+                                            id = R.color.gray03
+                                        ),
+                                        fontSize = 16.sp,
+                                        fontFamily = Roboto,
+                                        fontWeight = FontWeight.Normal
+                                    )
+                                }
+                            }
+                        }
+                    }
+                )
+
+                Image(
+                    modifier = Modifier
+                        .constrainAs(icon) {
+                            top.linkTo(parent.top)
+                            bottom.linkTo(parent.bottom)
+                            end.linkTo(textField.end, margin = 20.dp)
+                            height = Dimension.fillToConstraints
+                        }
+                        .size(24.dp)
+                        .toggleable(
+                            value = pwViewToggleState.value,
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = null,
+                            enabled = true,
+                            role = Role.Checkbox,
+                            onValueChange = {
+                                pwViewToggleState.value = it
+                            }
+                        ),
+                    painter = if (pwViewToggleState.value) painterResource(id = R.drawable.view)
+                    else painterResource(id = R.drawable.view_hide),
+                    contentDescription = "delete search text",
+                )
+
+            }
+        }
+    }
+
+    private fun setKeyboardDown() {
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        imm?.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 }
