390d3ae62bcda6bbc98576348a9eecbd3258c7a7
EunjeongChoi414 <giving0414@naver.com>
2021-12-03 11:38:08 +0900

    version 1.1.2 로 수정 및 release gradle 수정

--- app/src/main/java/wupitch/android/presentation/ui/main/my_activity/MyCrewDetailFragment.kt
+++ app/src/main/java/wupitch/android/presentation/ui/main/my_activity/MyCrewDetailFragment.kt
@@ -1,25 +1,26 @@
 package wupitch.android.presentation.ui.main.my_activity
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.StringRes
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.Dimension
-import androidx.core.app.Person.fromBundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
@@ -30,8 +31,8 @@ import kotlinx.coroutines.launch
 import wupitch.android.R
 import wupitch.android.presentation.theme.Roboto
 import wupitch.android.presentation.theme.WupitchTheme
-import wupitch.android.presentation.ui.components.*
-import wupitch.android.presentation.ui.main.home.crew_detail.components.JoinSuccessDialog
+import wupitch.android.presentation.ui.components.FullToolBar
+import wupitch.android.presentation.ui.components.ReportBottomSheetFragment
 import wupitch.android.presentation.ui.main.my_activity.components.*
 
 @ExperimentalPagerApi
